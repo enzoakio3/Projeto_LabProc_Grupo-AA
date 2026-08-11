09d09b518f95a99a93fb0ba8cac8bb27ef98e090
Pouhide <paulinhohide@outlook.com>
2026-08-11 13:01:23 -0300

    aumentar cvolume

--- tests/test_buzzer_pwm.py
+++ tests/test_buzzer_pwm.py
@@ -4,71 +4,19 @@ import time
 
 BUZZER_PIN = 4
 
-
-# =====================================================
-# CONFIGURAÇÃO
-# =====================================================
-
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(BUZZER_PIN, GPIO.OUT)
 
-
-# =====================================================
-# PWM
-# =====================================================
-
-pwm = GPIO.PWM(
-    BUZZER_PIN,
-    2000
-)
-
-# Começa com duty cycle alto
-pwm.start(80)
-
+pwm = GPIO.PWM(BUZZER_PIN, 2000)
 
 try:
+    print("Tocando buzzer em 2000 Hz...")
+    
+    pwm.start(50)
 
-    frequencies = [
-        1000,
-        1500,
-        1800,
-        2000,
-        2200,
-        2500,
-        3000
-    ]
-
-    for frequency in frequencies:
-
-        print(
-            f"Testando {frequency} Hz"
-        )
-
-        pwm.ChangeFrequency(
-            frequency
-        )
-
-        pwm.ChangeDutyCycle(
-            80
-        )
-
-        time.sleep(1.2)
-
-        # pausa
-        pwm.ChangeDutyCycle(
-            0
-        )
-
-        time.sleep(0.4)
-
+    time.sleep(5)
 
 finally:
-
     pwm.stop()
-
-    GPIO.output(
-        BUZZER_PIN,
-        GPIO.LOW
-    )
-
+    GPIO.output(BUZZER_PIN, GPIO.LOW)
     GPIO.cleanup()
